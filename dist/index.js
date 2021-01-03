b88ae2b9efd47551cb621a612056a7f4c55337b4
leafney <babycoolzx@126.com>
2021-01-03 13:10:11 +0800

    add actionCard

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,7 @@ const crypto = __webpack_require__(6417);
 
 const ACTION_PACKAGE = 'https://github.com/leafney/dingtalk-action';
 const DINGTALK_URL = 'https://oapi.dingtalk.com/robot/send';
+
 const VALID_MSGTYPES = ['text', 'link', 'markdown', 'actionCard', 'feedCard'];
 
 // most @actions toolkit packages have async methods
@@ -21,9 +22,7 @@ async function run() {
     const secret = process.env.DINGTALK_SECRET || '';
 
     const jobStatus = core.getInput('status');
-    const msgtype = core.getInput('msgtype', { required: true });
-
-    core.info(`环境变量及参数 accessToken:${accessToken} secret:${secret} jobStatus:${jobStatus} msgtype:${msgtype}`);
+    const msgtype = core.getInput('msgtype');
 
     if (accessToken == '') throw new Error('The environment variable parameter [DINGTALK_ACCESS_TOKEN] is required');
     if (jobStatus == '') throw new Error('The input parameter [status] is empty');
@@ -32,19 +31,38 @@ async function run() {
     let notifyWhen = core.getInput('notify_when');
     const title = core.getInput('title');
     const text = core.getInput('text');
+
+    // msgtype of text and markdown
     const atMobiles = core.getInput('at_mobiles');
     const atAll = (core.getInput('at_all') || 'false').toUpperCase() === 'TRUE';
 
-    core.info(`输入参数 notifyWhen:${notifyWhen} title:${title} text:${text} atMobiles:${atMobiles} atAll:${atAll}`);
-
-    // // msgtype of link
+    // msgtype of link
     const msgUrl = core.getInput('msg_url') || ACTION_PACKAGE;
     const picUrl = core.getInput('pic_url');
 
     // msgtype of actionCard
+    const singleTitle = core.getInput('single_title');
+    const singleUrl = core.getInput('single_url');
+    const btns = core.getInput('btns') || '[]';
+    const btnOrientation = core.getInput('btn_orientation') || '0';
+
+    let isOverAll = false;
+    if (singleTitle != '' || singleUrl != '') {
+      isOverAll = true;
+    }
+    singleTitle = singleTitle || 'Read More';
+    singleUrl = singleUrl || ACTION_PACKAGE;
+
+    const btnsObj = JSON.parse(btns);
+    if (!isOverAll) {
+      // btns 中必需包含 title 和 actionURL
+      if (!('title' in btnsObj) || !('actionURL' in btnsObj)) throw new Error('btns list should be exist [title] and [actionURL]');
+    }
+
     // msgtype of feedCard
     // const customCont = core.getInput('custom');
 
+    core.info(`输入参数 notifyWhen:${notifyWhen} singleTitle:${singleTitle} singleUrl:${singleUrl} btns:${btns} btnOrientation:${btnOrientation}`);
 
     let payload = { msgtype };
     switch (msgtype) {
@@ -80,6 +98,19 @@ async function run() {
           payload['at']['isAtAll'] = atAll;
         }
         break;
+      case 'actionCard':
+        payload['actionCard'] = {};
+        payload['actionCard']['title'] = title;
+        payload['actionCard']['text'] = text;
+        payload['actionCard']['btnOrientation'] = btnOrientation;
+
+        if (isOverAll) {
+          payload['actionCard']['singleTitle'] = singleTitle;
+          payload['actionCard']['singleURL'] = singleUrl;
+        } else {
+          payload['actionCard']['btns'] = btnsObj;
+        }
+        break;
       default:
         break;
     }
@@ -111,7 +142,6 @@ async function run() {
         }
       });
 
-      core.info('Dingtalk Robot Notify Response: ', ret.data);
       if (ret.data.errcode) {
         throw new Error(`Dingtalk Robot Notify Return Error: [${ret.data.errcode}] ${ret.data.message}`);
       }
